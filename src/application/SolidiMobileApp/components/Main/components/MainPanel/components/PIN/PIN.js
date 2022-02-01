7d47b645919510520779a3924c2beadfa4aa1469
StJohn Piano <stjohnpiano@gmail.com>
2022-02-01 19:30:51 +0100

    Add keychain test code

--- src/application/SolidiMobileApp/components/Main/components/MainPanel/components/PIN/PIN.js
+++ src/application/SolidiMobileApp/components/Main/components/MainPanel/components/PIN/PIN.js
@@ -19,6 +19,36 @@ import { screenWidth, screenHeight } from 'src/util/dimensions';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 
+import * as Keychain from 'react-native-keychain';
+
+let x = async () => {
+  const username = 'zuck';
+  const password = 'poniesRgr8';
+
+  console.log('=====')
+
+  // Store the credentials
+  await Keychain.setGenericPassword(username, password);
+
+  try {
+    // Retrieve the credentials
+    const credentials = await Keychain.getGenericPassword();
+    if (credentials) {
+      console.log(
+        'Credentials successfully loaded for user ' + credentials.username
+      );
+    } else {
+      console.log('No credentials stored');
+    }
+  } catch (error) {
+    console.log("Keychain couldn't be accessed!", error);
+  }
+  await Keychain.resetGenericPassword();
+};
+
+x();
+
+console.log('foo')
 
 
 
