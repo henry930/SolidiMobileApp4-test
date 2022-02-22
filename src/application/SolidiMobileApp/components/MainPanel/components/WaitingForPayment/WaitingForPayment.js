66427b2e6504ee8c767b2aafbc721599700ce1b1
StJohn Piano <stjohnpiano@gmail.com>
2022-02-22 12:51:10 +0100

    WaitingForPayment

---
+++ src/application/SolidiMobileApp/components/MainPanel/components/WaitingForPayment/WaitingForPayment.js
@@ -0,0 +1,15 @@
+// React imports
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+let WaitingForPayment = () => {
+
+  return (
+    <Text>WaitingForPayment</Text>
+  )
+
+}
+
+
+export default WaitingForPayment;
