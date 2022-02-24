084196f17b93f041834862e17f5409dbe8c7de1f
StJohn Piano <stjohnpiano@gmail.com>
2022-02-24 13:51:18 +0100

    Expand and restructure PaymentNotMade to handle PaymentNotReceived

--- src/application/SolidiMobileApp/components/MainPanel/components/PaymentNotMade/PaymentNotMade.js
+++ src/application/SolidiMobileApp/components/MainPanel/components/PaymentNotMade/PaymentNotMade.js
@@ -12,12 +12,28 @@ import { scaledWidth, scaledHeight, normaliseFont } from 'src/util/dimensions';
 import { Button, StandardButton, ImageButton } from 'src/components/atomic';
 import misc from 'src/util/misc';
 
+/* Notes
+- We use this component to also display "paymentNotReceived", if the user clicks "I have paid", but 2 hours pass without us receiving the payment.
+*/
+
 
 
 
 let PaymentNotMade = () => {
 
   let appState = useContext(AppStateContext);
+
+  let pageName = appState.pageName;
+  let permittedPageNames = 'default paymentNotReceived'.split(' ');
+  misc.confirmItemInArray('permittedPageNames', permittedPageNames, pageName, 'PaymentNotMade');
+
+  // Change some values, depending on the pageName.
+  let headingText = 'Payment not made';
+  let timePeriod = '30 minutes'
+  if (pageName == 'paymentNotReceived') {
+    headingText = 'Payment not received';
+    timePeriod = '2 hours'
+  }
 
   // Load order details.
   ({volumeQA, volumeBA, assetQA, assetBA} = appState.panels.buy);
@@ -44,13 +60,13 @@ let PaymentNotMade = () => {
     <View style={styles.panelSubContainer}>
 
       <View style={[styles.heading, styles.heading1]}>
-        <Text style={styles.headingText}>Payment not made</Text>
+        <Text style={styles.headingText}>{headingText}</Text>
       </View>
 
       <View style={styles.infoSection}>
 
         <View style={styles.infoItem}>
-          <Text style={styles.bold}>{`\u2022  `} 30 minutes have passed.</Text>
+          <Text style={styles.bold}>{`\u2022  `} {timePeriod} have passed.</Text>
         </View>
 
         <View style={styles.infoItem}>
@@ -63,16 +79,32 @@ let PaymentNotMade = () => {
 
       </View>
 
-      <View style={styles.textItem}>
-        <Text>Please note: If you made a payment, but used an incorrect reference or sent a different amount, the payment will be processed as a normal deposit. The funds will be added to your {assetsInfo[assetQA].displayString} account. In this case, you can now make a purchase of {assetsInfo[assetBA].displayString} directly, using the funds in your account.</Text>
+      <View style={styles.infoSection}>
+
+      <View style={styles.infoItem}>
+          <Text>Please note:</Text>
+        </View>
+
+        <View style={styles.infoItem}>
+          <Text>{`\u2022  `} If you made a payment, but used an incorrect reference or sent a different amount or waited too long to make it, the payment will be processed as a normal deposit.</Text>
+        </View>
+
+        <View style={styles.infoItem}>
+          <Text>{`\u2022  `} The funds will be added to your {assetsInfo[assetQA].displayString} account.</Text>
+        </View>
+
+        <View style={styles.infoItem}>
+          <Text>{`\u2022  `} In this case, you can now make a purchase of {assetsInfo[assetBA].displayString} directly, using the funds in your account.</Text>
+        </View>
+
+      </View>
+
+      <View style={styles.button}>
+        <StandardButton title="Go back to Buy" onPress={ buyAgain } />
       </View>
 
       <View style={styles.button}>
         <StandardButton title="View assets" onPress={ viewAssets } />
-      </View>
-
-      <View style={styles.button}>
-        <StandardButton title="Buy again" onPress={ buyAgain } />
       </View>
 
     </View>
@@ -115,12 +147,8 @@ let styles = StyleSheet.create({
   bold: {
     fontWeight: 'bold',
   },
-  textItem: {
-    marginTop: scaledHeight(20),
-    marginBottom: scaledHeight(20),
-  },
   button: {
-    marginVertical: scaledHeight(10),
+    marginTop: scaledHeight(20),
   },
 });
 
