1887c1d34b06450a84b1ca683abc7bc4a99cfe5c
StJohn Piano <stjohnpiano@gmail.com>
2022-03-04 17:38:24 +0100

    Expand Sell page.

--- src/application/SolidiMobileApp/components/MainPanel/components/Sell/Sell.js
+++ src/application/SolidiMobileApp/components/MainPanel/components/Sell/Sell.js
@@ -1,9 +1,27 @@
 // React imports
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useContext, useState, useEffect } from 'react';
+import { Text, TextInput, StyleSheet, View } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+
+// Other imports
+import _ from 'lodash';
+import Big from 'big.js';
+
+// Internal imports
+import AppStateContext from 'src/application/data';
+import { assetsInfo, mainPanelStates } from 'src/constants';
+import { StandardButton } from 'src/components/atomic';
+import { scaledWidth, scaledHeight, normaliseFont } from 'src/util/dimensions';
+import misc from 'src/util/misc';
+
+
 
 
 let Sell = () => {
+
+  let appState = useContext(AppStateContext);
+
+
 
   return (
     <Text>Sell</Text>
