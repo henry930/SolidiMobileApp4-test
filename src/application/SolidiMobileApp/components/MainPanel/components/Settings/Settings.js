5f81802ab225fd962646acb5ff36b0f0b49e55a4
StJohn Piano <stjohnpiano@gmail.com>
2022-03-20 19:32:18 +0100

    Settings: Remove personal details.

--- src/application/SolidiMobileApp/components/MainPanel/components/Settings/Settings.js
+++ src/application/SolidiMobileApp/components/MainPanel/components/Settings/Settings.js
@@ -30,153 +30,27 @@ let Settings = () => {
         <Text style={styles.headingText}>Settings</Text>
       </View>
 
-      <ScrollView style={styles.scrollView}>
+      <View style={styles.buttonWrapper}>
+        <StandardButton title='Lock App' onPress={ () => { appState.lockApp(); } } />
+      </View>
 
-        <View style={styles.sectionHeading}>
-          <Text style={styles.sectionHeadingText}>Personal Details</Text>
-        </View>
+      <View style={styles.buttonWrapper}>
+        <StandardButton title='Personal Details'
+          onPress={ () => { appState.changeState('PersonalDetails'); } }
+        />
+      </View>
 
-        <View style={styles.settingItem}>
-          <View style={styles.settingName}>
-            <Text style={styles.settingNameText}>{`\u2022  `}First Name</Text>
-          </View>
-          <View style={styles.settingValue}>
-            <Text style={styles.settingValueText}>{d.firstname}</Text>
-          </View>
-        </View>
+      <View style={styles.buttonWrapper}>
+        <StandardButton title='Bank Accounts'
+          onPress={ () => { appState.changeState('BankAccounts'); } }
+        />
+      </View>
 
-        <View style={styles.settingItem}>
-          <View style={styles.settingName}>
-            <Text style={styles.settingNameText}>{`\u2022  `}Last Name</Text>
-          </View>
-          <View style={styles.settingValue}>
-            <Text style={styles.settingValueText}>{d.lastname}</Text>
-          </View>
-        </View>
-
-        <View style={styles.settingItem}>
-          <View style={styles.settingName}>
-            <Text style={styles.settingNameText}>{`\u2022  `}Country of Citizenship</Text>
-          </View>
-          <View style={styles.settingValue}>
-            <Text style={styles.settingValueText}></Text>
-          </View>
-        </View>
-
-        <View style={styles.horizontalRule} />
-
-        <View style={styles.sectionHeading}>
-          <Text style={styles.sectionHeadingText}>Contact Details</Text>
-        </View>
-
-        <View style={styles.settingItem}>
-          <View style={styles.settingName}>
-            <Text style={styles.settingNameText}>{`\u2022  `}Email</Text>
-          </View>
-          <View style={styles.settingValue}>
-            <Text style={styles.settingValueText}>{d.email}</Text>
-          </View>
-        </View>
-
-        <View style={styles.settingItem}>
-          <View style={styles.settingName}>
-            <Text style={styles.settingNameText}>{`\u2022  `}Mobile Phone</Text>
-          </View>
-          <View style={styles.settingValue}>
-            <Text style={styles.settingValueText}>{d.mobile}</Text>
-          </View>
-        </View>
-
-        <View style={styles.settingItem}>
-          <View style={styles.settingName}>
-            <Text style={styles.settingNameText}>{`\u2022  `}Landline Phone</Text>
-          </View>
-          <View style={styles.settingValue}>
-            <Text style={styles.settingValueText}>{d.landline}</Text>
-          </View>
-        </View>
-
-        <View style={styles.horizontalRule} />
-
-        <View style={styles.sectionHeading}>
-          <Text style={styles.sectionHeadingText}>Address Details</Text>
-        </View>
-
-        <View style={styles.settingItem}>
-          <View style={styles.settingName}>
-            <Text style={styles.settingNameText}>{`\u2022  `}Address</Text>
-          </View>
-          <View style={styles.settingValue}>
-            <Text style={styles.settingValueText}>{d.address_1}</Text>
-          </View>
-        </View>
-
-        <View style={styles.settingItem}>
-          <View style={styles.settingName}>
-            <Text style={styles.settingNameText}></Text>
-          </View>
-          <View style={styles.settingValue}>
-            <Text style={styles.settingValueText}>{d.address_2}</Text>
-          </View>
-        </View>
-
-        <View style={styles.settingItem}>
-          <View style={styles.settingName}>
-            <Text style={styles.settingNameText}></Text>
-          </View>
-          <View style={styles.settingValue}>
-            <Text style={styles.settingValueText}>{d.address_3}</Text>
-          </View>
-        </View>
-
-        <View style={styles.settingItem}>
-          <View style={styles.settingName}>
-            <Text style={styles.settingNameText}></Text>
-          </View>
-          <View style={styles.settingValue}>
-            <Text style={styles.settingValueText}>{d.address_4}</Text>
-          </View>
-        </View>
-
-        <View style={styles.settingItem}>
-          <View style={styles.settingName}>
-            <Text style={styles.settingNameText}>Postcode</Text>
-          </View>
-          <View style={styles.settingValue}>
-            <Text style={styles.settingValueText}>{d.postcode}</Text>
-          </View>
-        </View>
-
-        <View style={styles.settingItem}>
-          <View style={styles.settingName}>
-            <Text style={styles.settingNameText}>Country</Text>
-          </View>
-          <View style={styles.settingValue}>
-            <Text style={styles.settingValueText}>{d.country}</Text>
-          </View>
-        </View>
-
-        <View style={styles.horizontalRule} />
-
-        <View style={styles.sectionHeading}>
-          <Text style={styles.sectionHeadingText}>Security Details</Text>
-        </View>
-
-        <View style={styles.settingItem}>
-          <View style={styles.settingName}>
-            <Text style={styles.settingNameText}>PIN</Text>
-          </View>
-          <View style={styles.settingValue}>
-            <Text style={styles.settingValueText}>{appState.user.pin}</Text>
-          </View>
-          <View style={styles.buttonWrapper}>
-            <StandardButton title='Change PIN' onPress={ () => { appState.choosePIN(); } } />
-          </View>
-        </View>
-
-
-
-        </ScrollView>
+      <View style={styles.buttonWrapper}>
+        <StandardButton title='Security'
+          onPress={ () => { appState.changeState('Security'); } }
+        />
+      </View>
 
     </View>
     </View>
@@ -201,67 +75,24 @@ let styles = StyleSheet.create({
   },
   heading1: {
     marginTop: scaledHeight(10),
+    marginBottom: scaledHeight(30),
   },
   headingText: {
     fontSize: normaliseFont(20),
     fontWeight: 'bold',
   },
-  scrollView: {
-    //borderWidth: 1, // testing
-    height: '90%',
-  },
   bold: {
     fontWeight: 'bold',
-  },
-  sectionHeading: {
-    marginTop: scaledHeight(20),
-    marginBottom: scaledHeight(20),
-  },
-  sectionHeadingText: {
-    fontSize: normaliseFont(16),
-    fontWeight: 'bold',
-  },
-  settingItem: {
-    //borderWidth: 1, // testing
-    marginBottom: scaledHeight(10),
-    flexDirection: 'row',
-    flexWrap: 'wrap', // Allows long setting value to move onto the next line.
-  },
-  settingName: {
-    paddingRight: scaledWidth(10),
-    paddingVertical: scaledHeight(10),
-    //borderWidth: 1, // testing
-    minWidth: '40%', // Expands with length of setting name.
-  },
-  settingNameText: {
-    fontSize: normaliseFont(16),
-    fontWeight: 'bold',
-  },
-  settingValue: {
-    paddingLeft: scaledWidth(10),
-    paddingVertical: scaledHeight(10),
-    borderWidth: 1,
-    borderRadius: 16,
-    borderColor: colors.greyedOutIcon,
-    minWidth: '50%',
-  },
-  settingValueText: {
-    fontSize: normaliseFont(16),
-  },
-  horizontalRule: {
-    borderWidth: 1,
-    borderBottomColor: 'black',
-    borderBottomWidth: 1,
-    marginTop: scaledWidth(20),
-    marginHorizontal: scaledWidth(20),
   },
   buttonWrapper: {
     paddingRight: scaledWidth(40),
     marginVertical: scaledHeight(10),
     width: '100%',
+  },
+  alignRight: {
     flexDirection: 'row',
     justifyContent: 'flex-end',
-  }
+  },
 });
 
 
