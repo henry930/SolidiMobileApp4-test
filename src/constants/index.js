028e10acdc5e660419054e52a811f2dadb8bb332
StJohn Piano <stjohnpiano@gmail.com>
2022-03-24 19:19:35 +0100

    Remove hardcoded assetsInfo

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,9 @@
-import assetsInfo from './assetsInfo';
 import colors from './colors';
 import footerButtonList from './footerButtonList';
 import footerIcons from './footerIcons';
 import mainPanelStates from './mainPanelStates';
 
 export {
-  assetsInfo,
   colors,
   footerButtonList,
   footerIcons,
