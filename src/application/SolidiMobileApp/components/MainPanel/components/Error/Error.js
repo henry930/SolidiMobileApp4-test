9383a6ff2ddf0fd9fa66e4d8190fb83d0d5b63cc
StJohn Piano <stjohnpiano@gmail.com>
2022-04-06 11:44:58 +0200

    Error page

---
+++ src/application/SolidiMobileApp/components/MainPanel/components/Error/Error.js
@@ -0,0 +1,132 @@
+// React imports
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+// Other imports
+import _ from 'lodash';
+import Big from 'big.js';
+
+// Internal imports
+import AppStateContext from 'src/application/data';
+import { mainPanelStates, colors } from 'src/constants';
+import { scaledWidth, scaledHeight, normaliseFont } from 'src/util/dimensions';
+import { Button, StandardButton, ImageButton, Spinner } from 'src/components/atomic';
+import misc from 'src/util/misc';
+
+// Logger
+import logger from 'src/util/logger';
+let logger2 = logger.extend('Error');
+let {deb, dj, log, lj} = logger.getShortcuts(logger2);
+
+
+/* Notes
+
+
+*/
+
+
+
+
+let Error = () => {
+
+  let appState = useContext(AppStateContext);
+  let [renderCount, triggerRender] = useState(0);
+  let firstRender = misc.useFirstRender();
+  let stateChangeID = appState.stateChangeID;
+
+  let pageName = appState.pageName;
+  let permittedPageNames = 'default'.split(' ');
+  misc.confirmItemInArray('permittedPageNames', permittedPageNames, pageName, 'Error');
+
+
+  // Initial setup.
+  useEffect( () => {
+    setup();
+  }, []); // Pass empty array so that this only runs once on mount.
+
+
+  let setup = async () => {
+    // await appState.loadUserInfo();
+    if (appState.stateChangeIDHasChanged(stateChangeID)) return;
+    triggerRender(renderCount+1);
+  }
+
+
+  return (
+    <View style={styles.panelContainer}>
+    <View style={styles.panelSubContainer}>
+
+      <View style={[styles.heading, styles.heading1]}>
+        <Text style={styles.headingText}>Error</Text>
+      </View>
+
+      <Text style={styles.bold}>{'\n'}Unfortunately, an error has occurred.</Text>
+
+      <View style={styles.infoSection}>
+
+        <View style={styles.infoItem}>
+          <Text style={styles.bold}>Error message:</Text>
+          <Text>{'\n'}{appState.error.message}</Text>
+        </View>
+
+      </View>
+
+      <View style={styles.infoSection}>
+          <Text>Please take a screenshot of this page in order to record the error message, and then:{'\n'}</Text>
+          <Button title="Contact Us"
+            onPress={ () => { appState.changeState('ContactUs') } }
+            styles={styleContactUsButton}
+          />
+      </View>
+
+    </View>
+    </View>
+  )
+
+}
+
+
+let styles = StyleSheet.create({
+  panelContainer: {
+    paddingHorizontal: scaledWidth(15),
+    paddingVertical: scaledHeight(5),
+    width: '100%',
+    height: '100%',
+  },
+  panelSubContainer: {
+    paddingTop: scaledHeight(10),
+    paddingHorizontal: scaledWidth(30),
+  },
+  heading: {
+    alignItems: 'center',
+  },
+  heading1: {
+    marginTop: scaledHeight(10),
+  },
+  headingText: {
+    fontSize: normaliseFont(20),
+    fontWeight: 'bold',
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  infoSection: {
+    paddingTop: scaledHeight(20),
+    alignItems: 'flex-start',
+  },
+  infoItem: {
+    marginBottom: scaledHeight(5),
+  },
+});
+
+
+let styleContactUsButton = StyleSheet.create({
+  text: {
+    margin: 0,
+    padding: 0,
+    fontSize: normaliseFont(14),
+  },
+});
+
+
+export default Error;
