5699460088807ffa4480a2ccba92881c1f52a183
StJohn Piano <stjohnpiano@gmail.com>
2022-05-05 13:30:47 +0200

    Load Register page in webview. Listen for event that shows completion. Load access keys for basic auth from uncommited file.

--- src/application/SolidiMobileApp/components/MainPanel/components/Register/Register.js
+++ src/application/SolidiMobileApp/components/MainPanel/components/Register/Register.js
@@ -1,7 +1,7 @@
 // React imports
 import React, { useContext, useEffect, useRef, useState } from 'react';
 import { Text, StyleSheet, View } from 'react-native';
-//import { WebView } from 'react-native-webview';
+import { WebView } from 'react-native-webview';
 
 // Other imports
 import _ from 'lodash';
@@ -22,7 +22,8 @@ let {deb, dj, log, lj} = logger.getShortcuts(logger2);
 
 /* Notes
 
-<WebView source={{ uri: 'https://reactnative.dev/' }} />
+Guide:
+https://github.com/react-native-webview/react-native-webview
 
 */
 
@@ -39,6 +40,17 @@ let Register = () => {
   let pageName = appState.pageName;
   let permittedPageNames = 'default'.split(' ');
   misc.confirmItemInArray('permittedPageNames', permittedPageNames, pageName, 'Register');
+
+
+  // Settings
+  let basicAuth = '';
+  if (appState.appTier == 'dev') {
+    // A dev Solidi instance is behind a basic authentication barrier.
+    let username = appState.devBasicAuth.username;
+    let password = appState.devBasicAuth.password;
+    basicAuth = `${username}:${password}@`;
+  }
+  uriSolidiWebsiteRegister = `https://${basicAuth}${appState.domain}/register`;
 
 
   // Initial setup.
@@ -67,15 +79,29 @@ let Register = () => {
         <Text style={styles.headingText}>Register</Text>
       </View>
 
-      <View style={styles.buttonWrapper}>
-        <Button title="Already have an account? Click here."
-          onPress={ () => { appState.changeState('Login') } }
-        />
-      </View>
-
       <View style={styles.webviewSection}>
 
-
+        <WebView
+          source={{ uri: uriSolidiWebsiteRegister }}
+          startInLoadingState={true}
+          renderLoading={() =>
+            <View style={styles.loadingView}>
+              <Spinner/>
+            </View>
+          }
+          onMessage={(event) => {
+            let data = event.nativeEvent.data;
+            log(`Have received event from embedded webview, containing data = '${data}'`);
+            if (data == 'Successful login') {
+              /* Explanation:
+              - This event is emitted by the front-end website in the callback that executes after a successful login.
+              - After a successful registration, the front-end will log in using the supplied details.
+              - We pick up the event here, and change to the Login screen.
+              */
+              appState.changeState('Login');
+            }
+          }}
+        />
 
       </View>
 
@@ -88,21 +114,25 @@ let Register = () => {
 
 let styles = StyleSheet.create({
   panelContainer: {
-    paddingHorizontal: scaledWidth(15),
+    //paddingHorizontal: scaledWidth(15),
+    paddingHorizontal: scaledWidth(0),
     paddingVertical: scaledHeight(5),
     width: '100%',
     height: '100%',
   },
   panelSubContainer: {
     paddingTop: scaledHeight(10),
-    paddingHorizontal: scaledWidth(30),
+    //paddingHorizontal: scaledWidth(30),
+    paddingHorizontal: scaledWidth(0),
   },
+
   heading: {
     alignItems: 'center',
   },
   heading1: {
     marginTop: scaledHeight(10),
-    marginBottom: scaledHeight(40),
+    //marginBottom: scaledHeight(40),
+    marginBottom: scaledHeight(10),
   },
   headingText: {
     fontSize: normaliseFont(20),
@@ -112,12 +142,16 @@ let styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   buttonWrapper: {
-    marginTop: scaledHeight(0),
+    marginTop: scaledHeight(10),
     //borderWidth: 1, // testing
   },
   webviewSection: {
-    marginTop: scaledHeight(10),
-    borderWidth: 1, // testing
+    //borderWidth: 1, // testing
+    height: scaledHeight(550),
+    width: '100%',
+  },
+  loadingView: {
+    height: '100%',
   },
 });
 
