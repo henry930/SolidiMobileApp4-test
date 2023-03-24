5a65c3f51505cecbb518ee1d8399ffbad8b7cf83
StJohn Piano <stjohnpiano@gmail.com>
2023-03-24 17:25:26 +0100

    Streamline MainPanel.js

--- src/application/SolidiMobileApp/components/MainPanel/MainPanel.js
+++ src/application/SolidiMobileApp/components/MainPanel/MainPanel.js
@@ -10,116 +10,64 @@ import {
 import _ from 'lodash';
 
 // Internal imports
-import { colors, mainPanelStates } from 'src/constants';
-import { Test, Buy, Sell, Send, Receive, Assets, History,
-  Notifications, Settings, Login, PIN, ChooseHowToPay,
-  MakePayment, WaitingForPayment, BlankExampleComponent,
-  PaymentNotMade, PurchaseSuccessful, InsufficientBalance,
-  ReadArticle, ChooseHowToReceivePayment, RequestTimeout,
-  SaleSuccessful, PersonalDetails, ContactUs, BankAccounts,
-  Security, RequestFailed, Error, SendSuccessful, Authenticate,
-  Register, SupportTools, LimitsExceeded, IdentityVerification,
-  ResetPassword, MakePaymentOpenBanking, SolidiAccount,
-  CloseSolidiAccount, RegisterConfirm, RegisterConfirm2,
-  AccountUpdate } from './components';
 import AppStateContext from 'src/application/data';
+import { colors, mainPanelStates } from 'src/constants';
+import misc from 'src/util/misc';
+import * as allPanels from './components/index';
 
-import * as allcomponents from './components';
+// Logger
+import logger from 'src/util/logger';
+let logger2 = logger.extend('AccountUpdate');
+let {deb, dj, log, lj} = logger.getShortcuts(logger2);
+
+// Shortcuts
+let jd = JSON.stringify;
+
+
 
 
 let MainPanel = (props) => {
 
-  let {style:styleArg} = props;
+
+  let {style: styleArg} = props;
 
   let appState = useContext(AppStateContext);
 
+
   let selectPanelComponent = () => {
 
-    // JDM - 2023-03-07 - Do we need to use mainPanelState here? We've already check this in AppState.changeState.
-    if (mainPanelStates.indexOf(appState.mainPanelState)==-1) {
-      return <Text>Early Error in MainPanel.js: Unknown mainPanelState: {appState.mainPanelState}</Text>
+    let {mainPanelState, pageName} = appState;
+
+    // appState.changeState also checks for an unrecognised panel, but we check here too, just in case. Other functions besides changeState can be used to change the panel.
+    if (! mainPanelStates.includes(mainPanelState)) {
+      return <Text>Error in MainPanel.js: mainPanelState '{}' not found in src/constants/mainPanelStates.js. </Text>
     }
-    // JDM - 2023-03-07 - Alternative way to check for valid components (maybe too flexible?)
-    if(!Object.keys(allcomponents).includes(appState.mainPanelState)) {
-      return <Text>Error in MainPanel.js: Unknown mainPanelState: {appState.mainPanelState}</Text>
+
+    // Check that there is a component file with the specified name.
+    let componentNames = _.keys(allPanels);
+    if(! componentNames.includes(mainPanelState)) {
+      return <Text>Error in MainPanel.js: No component file found in MainPanel/components with the same name as mainPanelState '{mainPanelState}'.</Text>
     }
 
     // Special cases
-    if (appState.mainPanelState === 'PIN') {
-      if (appState.pageName == 'default') {
+    if (mainPanelState === 'PIN') {
+      if (pageName == 'default') {
         if (! appState.user.pin) {
           return <Login />
         }
       }
       return <PIN />
-    } else if (appState.mainPanelState === 'ChooseHowToPay') {
-      return <ChooseHowToPay />
-    } else if (appState.mainPanelState === 'MakePayment') {
-      return <MakePayment />
-    } else if (appState.mainPanelState === 'WaitingForPayment') {
-      return <WaitingForPayment />
-    } else if (appState.mainPanelState === 'BlankExampleComponent') {
-      return <BlankExampleComponent />
-    } else if (appState.mainPanelState === 'PaymentNotMade') {
-      return <PaymentNotMade />
-    } else if (appState.mainPanelState === 'PurchaseSuccessful') {
-      return <PurchaseSuccessful />
-    } else if (appState.mainPanelState === 'InsufficientBalance') {
-      return <InsufficientBalance />
-    } else if (appState.mainPanelState === 'ReadArticle') {
-      return <ReadArticle />
-    } else if (appState.mainPanelState === 'ChooseHowToReceivePayment') {
-      return <ChooseHowToReceivePayment />
-    } else if (appState.mainPanelState === 'RequestTimeout') {
-      return <RequestTimeout />
-    } else if (appState.mainPanelState === 'SaleSuccessful') {
-      return <SaleSuccessful />
-    } else if (appState.mainPanelState === 'PersonalDetails') {
-      return <PersonalDetails />
-    } else if (appState.mainPanelState === 'ContactUs') {
-      return <ContactUs />
-    } else if (appState.mainPanelState === 'BankAccounts') {
-      return <BankAccounts />
-    } else if (appState.mainPanelState === 'Security') {
-      return <Security />
-    } else if (appState.mainPanelState === 'RequestFailed') {
-      return <RequestFailed />
-    } else if (appState.mainPanelState === 'Error') {
-      return <Error />
-    } else if (appState.mainPanelState === 'SendSuccessful') {
-      return <SendSuccessful />
-    } else if (appState.mainPanelState === 'Authenticate') {
-      return <Authenticate />
-    } else if (appState.mainPanelState === 'Register') {
-      return <Register />
-    } else if (appState.mainPanelState === 'SupportTools') {
-      return <SupportTools />
-    } else if (appState.mainPanelState === 'LimitsExceeded') {
-      return <LimitsExceeded />
-    } else if (appState.mainPanelState === 'IdentityVerification') {
-      return <IdentityVerification />
-    } else if (appState.mainPanelState === 'ResetPassword') {
-      return <ResetPassword />
-    } else if (appState.mainPanelState === 'MakePaymentOpenBanking') {
-      return <MakePaymentOpenBanking />
-    } else if (appState.mainPanelState === 'SolidiAccount') {
-      return <SolidiAccount />
-    } else if (appState.mainPanelState === 'CloseSolidiAccount') {
-      return <CloseSolidiAccount />
-    } else if (appState.mainPanelState === 'RegisterConfirm') {
-      return <RegisterConfirm />
-    } else if (appState.mainPanelState === 'RegisterConfirm2') {
-      return <RegisterConfirm2 />
-    } else if (appState.mainPanelState === 'AccountUpdate') {
-      return <AccountUpdate />
-    } else {
-      return <Text>Error in MainPanel.js: Unknown mainPanelState: {appState.mainPanelState}</Text>
     }
-    // Get the component that the mainpanel is set to (text string) from all the components.
-    // and then create the JSX element and return it.
-    let targetcomponent = allcomponents[appState.mainPanelState]
-    return React.createElement(targetcomponent);
+
+    /*
+    - appState.mainPanelState is a text string - we use it as a key to get the component from allPanels.
+    - Use the mainPanelState to select a component from allPanels.
+    - Create the React element from the component and return it.
+    */
+    let selectedComponent = allPanels[appState.mainPanelState];
+    return React.createElement(selectedComponent);
   }
+
 
   return (
       <View style={[styleArg, styles.mainPanel]}>
